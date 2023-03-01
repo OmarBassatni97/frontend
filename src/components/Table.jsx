c82441c5a7edc2743af0a9a6d52b51043a516f69
Omar Bassatni <omarbassatni@gmail.com>
2023-03-01 14:01:34 +0200

    created table filter

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import TableFilters from './TableFilters'
 import TableHeader from './TableHeader'
 
 const Table = () => {
     return (
         <div className='max-w-[1440px] mx-auto mt-4'>
             <TableHeader />
+            <TableFilters />
         </div>
     )
 }
---
+++ src/components/TableFilters.jsx
@@ -0,0 +1,41 @@
+import Image from 'next/image'
+import React from 'react'
+
+const TableFilters = () => {
+    return (
+        <div className='flex items-center justify-between mt-4 gap-3'>
+            <div className='flex gap-3'>
+                <span className='flex gap-1'>50 <Image src='/Assets/Vector 55.svg' height={6} width={6} alt='vector' /> Entries</span>
+                <div className='border-x px-3'>
+                    <Image
+                        src='/Assets/search.svg'
+                        width={16}
+                        height={16}
+                        alt='search'
+                    />
+                </div>
+                <div className='px-2 flex gap-1'>Client Manager <Image src='/Assets/Vector 55.svg' height={4} width={6} alt='vector' /></div>
+                <div className='px-2 flex gap-1'>Create Date <Image src='/Assets/Vector 55.svg' height={4} width={6} alt='vector' /></div>
+                <div className='px-2 flex gap-1'>Last activity date <Image src='/Assets/Vector 55.svg' height={4} width={6} alt='vector' /></div>
+                <div className='px-2 flex gap-1'>Lead Status <Image src='/Assets/Vector 55.svg' height={4} width={6} alt='vector' /></div>
+                <div className='px-2 flex gap-1'>Custom Filter <Image src='/Assets/Vector 55.svg' height={4} width={6} alt='vector' /></div>
+            </div>
+
+
+            <div className='flex gap-4'>
+                <Image
+                    src='/Assets/trash-2.svg'
+                    height={24}
+                    width={24}
+                    alt='trash' />
+                <Image
+                    src='/Assets/refresh-circle.svg'
+                    height={24}
+                    width={24}
+                    alt='refresh' />
+            </div>
+        </div>
+    )
+}
+
+export default TableFilters
