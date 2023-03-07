3bc90bdeaa7422819e606aa0d363ecdd9658d1fe
Omar Bassatni <omarbassatni@gmail.com>
2023-03-07 14:48:53 +0200

    adding errors

--- src/components/MultiSelect.jsx
+++ src/components/MultiSelect.jsx
@@ -2,20 +2,25 @@ import React from 'react'
 import { useFormContext, Controller } from 'react-hook-form';
 import Select from 'react-select';
 const MultiSelect = ({ options, name }) => {
-    const { control } = useFormContext();
+    const { control, formState } = useFormContext();
+    const { errors } = formState
     return (
-        <Controller
-            control={control}
-            name={name}
-            render={({ field }) => (
-                <Select
-                    className='my-2'
-                    {...field}
-                    isMulti
-                    options={options}
-                />
-            )}
-        />
+        <>
+            <Controller
+                control={control}
+                name={name}
+                render={({ field }) => (
+                    <Select
+                        className='my-2'
+                        {...field}
+                        isMulti
+                        options={options}
+                    />
+                )}
+            />
+            {errors[name] ? <p className='text-red-300'>Required</p> : ''}
+        </>
+
 
     )
 }
--- src/components/SingleSelect.jsx
+++ src/components/SingleSelect.jsx
@@ -2,21 +2,26 @@ import React from 'react'
 import { Controller, useFormContext } from 'react-hook-form';
 import Select from 'react-select';
 const SingleSelect = ({ options, name }) => {
-    const { control } = useFormContext();
+    const { control, formState } = useFormContext();
+    const { errors } = formState
     return (
-        <Controller
-            control={control}
-            name={name}
-            render={({ field }) => (
-                <Select
-                    className='my-2'
-                    {...field}
-                    isClearable
-                    defaultValue={options[0]}
-                    options={options}
-                />
-            )}
-        />
+        <>
+            <Controller
+                control={control}
+                name={name}
+                render={({ field }) => (
+                    <Select
+                        className='my-2'
+                        {...field}
+                        isClearable
+                        defaultValue={options[0]}
+                        options={options}
+                    />
+                )}
+            />
+            {errors[name] ? <p className='text-red-300'>Required</p> : ''}
+        </>
+
     )
 }
 
