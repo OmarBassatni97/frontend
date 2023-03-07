7e26a728346d3bb2df83295429354d39af712180
Omar Bassatni <omarbassatni@gmail.com>
2023-03-07 14:43:29 +0200

    adding errors

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { useFormContext } from 'react-hook-form';
 
 const InputField = ({ label, name }) => {
-    const { register } = useFormContext();
+    const { register, formState } = useFormContext();
+    const { errors } = formState
     return (
         <div className='my-2'>
             <span>{label}</span>
             <input className='w-full p-2 border' type="text"  {...register(name)} />
+            {errors[name] ? <p className='text-red-400'>First Name is required</p> : ''}
         </div>
     )
 }
